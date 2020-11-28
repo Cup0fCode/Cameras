af65a34ef2bbdf0ced2c16e00aa3b0681d5aeed9
Matt <gr8bitmail@gmail.com>
2020-11-28 15:30:26 -0600

    Added remote dl of resource packs

--- Cameras/src/water/of/cup/ResourcePackManager.java
+++ Cameras/src/water/of/cup/ResourcePackManager.java
@@ -5,8 +5,11 @@ import org.bukkit.Material;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.URL;
 import java.util.HashMap;
 
 public class ResourcePackManager {
@@ -21,18 +24,18 @@ public class ResourcePackManager {
             mapDir.mkdir();
         }
 
-        // TODO: Look for and download resource packs
-
         File folder = new File(dataFolder + "/resource-packs");
         File[] listOfFiles = folder.listFiles();
 
         if(listOfFiles.length == 0) {
             Bukkit.getLogger().info("No resource pack found, downloading...");
+            this.downloadResourcePack();
         } else {
             this.resourcePackFile = listOfFiles[0];
-            this.initializeImageHashmap();
-            Bukkit.getLogger().info("Using resource pack " + resourcePackFile.getName());
         }
+
+        this.initializeImageHashmap();
+        Bukkit.getLogger().info("Using resource pack " + this.resourcePackFile.getName());
     }
 
     public File getTextureByMaterial(Material material) {
@@ -68,11 +71,33 @@ public class ResourcePackManager {
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-
             }
         }
 
         Bukkit.getLogger().info("Loaded " + this.imageHashMap.size() + " textures from resource pack " + this.resourcePackFile.getName());
+    }
+
+    private void downloadResourcePack() {
+        File destLocation = new File(Camera.getInstance().getDataFolder() + "/resource-packs/1_16_4/");
+        File fileLocation = new File(Camera.getInstance().getDataFolder() + "/resource-packs/1_16_4.zip");
+        try (BufferedInputStream in = new BufferedInputStream(new URL("https://github.com/Cup0fCode/resource-packs/raw/main/1_16_4.zip").openStream());
+            FileOutputStream fileOutputStream = new FileOutputStream(fileLocation)) {
+            byte dataBuffer[] = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+                fileOutputStream.write(dataBuffer, 0, bytesRead);
+            }
+
+            if (!destLocation.exists()) {
+                destLocation.mkdir();
+            }
+
+            ZipUtils.unzip(fileLocation, Camera.getInstance().getDataFolder() + "/resource-packs/1_16_4/");
+            fileLocation.delete();
+            this.resourcePackFile = destLocation;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public HashMap<Material, BufferedImage> getImageHashMap() {
