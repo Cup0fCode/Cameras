31f78db998b3e8cc7be8b34d32cfe414a64f8808
Matt <gr8bitmail@gmail.com>
2020-11-27 22:25:02 -0600

    Added nms debug command

--- Cameras/src/water/of/cup/commands/CameraCommands.java
+++ Cameras/src/water/of/cup/commands/CameraCommands.java
@@ -1,8 +1,12 @@
 package water.of.cup.commands;
 
+import net.minecraft.server.v1_14_R1.MinecraftServer;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.craftbukkit.v1_14_R1.CraftServer;
 import org.bukkit.entity.Player;
 
 import water.of.cup.Picture;
@@ -15,6 +19,13 @@ public class CameraCommands implements CommandExecutor {
             return true;
         }
         Player p = (Player) sender;
+
+        if(args.length > 0 && args[0].equalsIgnoreCase("nms")) {
+            p.sendMessage(ChatColor.RED + "NMS debugging");
+            MinecraftServer minecraftServer = ((CraftServer) Bukkit.getServer()).getServer();
+//            minecraftServer.
+            return true;
+        }
 
         if (cmd.getName().equalsIgnoreCase("takepicture") && p.isOp()) {
             
